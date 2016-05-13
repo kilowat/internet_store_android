480d6f1fb8991692271277d66e6902fdf1611460
kilowat <kadantsev.sergey@gmail.com>
2016-05-13 16:05:18 +0300

    init

---
+++ app/src/main/java/pro/aliens/intenrent_store_android/adapter/ProductAdater.java
@@ -0,0 +1,9 @@
+package pro.aliens.intenrent_store_android.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Admin on 13.05.2016.
+ */
+public class ProductAdater {
+}
