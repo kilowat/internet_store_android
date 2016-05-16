c80cadb98f6822e38a8d2177c457d7714a6a713a
kilowat <kadantsev.sergey@gmail.com>
2016-05-16 17:19:26 +0300

    init

--- app/src/main/java/pro/aliens/intenrent_store_android/activity/ProductListActivity.java
+++ app/src/main/java/pro/aliens/intenrent_store_android/activity/ProductListActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import pro.aliens.intenrent_store_android.R;
 import pro.aliens.intenrent_store_android.Utils.DataGenerator;
 import pro.aliens.intenrent_store_android.adapter.ProductAdater;
+import pro.aliens.intenrent_store_android.model.Product;
 
-public class ProductListActivity extends BaseActivity {
+public class ProductListActivity extends BaseActivity implements ProductAdater.OnProductClickListener {
 
     protected ProductAdater productAdater;
     protected RecyclerView productRecycler;
@@ -24,6 +27,12 @@ public class ProductListActivity extends BaseActivity {
         productRecycler = (RecyclerView) findViewById(R.id.product_list_recycler);
         productRecycler.setLayoutManager(new GridLayoutManager(this, countRow));
         productAdater = new ProductAdater(this, DataGenerator.products());
+        productAdater.setOnClickListener(this);
         productRecycler.setAdapter(productAdater);
+    }
+
+    @Override
+    public void onClick(View view, Product product) {
+        Toast.makeText(this, product.getName(),Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/pro/aliens/intenrent_store_android/adapter/ProductAdater.java
+++ app/src/main/java/pro/aliens/intenrent_store_android/adapter/ProductAdater.java
@@ -1,10 +1,12 @@
 package pro.aliens.intenrent_store_android.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,11 +18,13 @@ import pro.aliens.intenrent_store_android.model.Product;
 /**
  * Created by Admin on 13.05.2016.
  */
-public class ProductAdater extends RecyclerView.Adapter<ProductAdater.ProductViewHolder> {
+public class ProductAdater extends RecyclerView.Adapter<ProductAdater.ProductViewHolder>  {
 
     private LayoutInflater layoutInflater;
     private Context context;
     private ArrayList<Product> products = new ArrayList<>();
+    private OnProductClickListener onProductClickListener;
+
 
     public ProductAdater(Context context, ArrayList<Product> products) {
         this.context = context;
@@ -32,8 +36,19 @@ public class ProductAdater extends RecyclerView.Adapter<ProductAdater.ProductVie
     public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View view = layoutInflater.inflate(R.layout.product_list, parent, false);
-        ProductViewHolder productViewHolder = new ProductViewHolder(view);
+        Button addToCard = (Button) view.findViewById(R.id.add_to_cart);
 
+        final ProductViewHolder productViewHolder = new ProductViewHolder(view);
+
+        addToCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(onProductClickListener!=null) {
+                    Product curProduct = products.get(productViewHolder.getAdapterPosition());
+                    onProductClickListener.onClick(v, curProduct);
+                }
+            }
+        });
         return productViewHolder;
     }
 
@@ -55,6 +70,13 @@ public class ProductAdater extends RecyclerView.Adapter<ProductAdater.ProductVie
         return products.size();
     }
 
+
+    public void setOnClickListener(OnProductClickListener onProductClickListener){
+
+        this.onProductClickListener = onProductClickListener;
+    }
+
+
     class ProductViewHolder extends RecyclerView.ViewHolder{
 
         public ImageView imageView;
@@ -68,5 +90,9 @@ public class ProductAdater extends RecyclerView.Adapter<ProductAdater.ProductVie
             nameView = (TextView) itemView.findViewById(R.id.product_name);
             priceView = (TextView) itemView.findViewById(R.id.product_price);
         }
+    }
+
+    public interface OnProductClickListener{
+        public void onClick(View view, Product product);
     }
 }
